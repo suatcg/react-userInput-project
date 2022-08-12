5c63ca3bb05748bc1fadf9808bf0a397fe599041
suatcg <suat-can_24@hotmail.com>
2022-08-12 19:32:44 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [{ id: Math.random().toString(), name: uName, age: uAge }, ...prevUsersList];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
